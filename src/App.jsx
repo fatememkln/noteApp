6f94a5a8613ef92d2b4fcf257f7054d70667f4bd
fatememkln <fateme.mkln@gmail.com>
2024-04-28 19:19:06 +0330

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,54 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "./App.css";
 import AddNewNote from "./components/addNewNote";
 import NoteList from "./components/noteList";
 import NoteStatus from "./components/noteStatus";
 import NoteHeader from "./components/noteHeader";
 
+function notesReducer(notes, { type, payload }) {
+  switch (type) {
+    case "add": {
+      return [...notes, payload];
+    }
+    case "delete": {
+      return notes.filter((n) => n.id !== payload);
+    }
+    case "completed": {
+      return notes.map((note) =>
+        note.id === payload ? { ...note, completed: !note.completed } : note
+      );
+    }
+    default:
+      throw new Error("Known Error" + type);
+  }
+}
+
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(notesReducer, []);
+
+  // const [notes, setNotes] = useState([]);
   const [sortBy, setSortBy] = useState("latest");
 
   const handleAddNote = (newNote) => {
-    setNotes((prevNotes) => [...prevNotes, newNote]);
+    // setNotes((prevNotes) => [...prevNotes, newNote]);
+    dispatch({ type: "add", payload: newNote });
   };
 
   const handleDeleteNote = (id) => {
-    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+    // setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+    dispatch({ type: "delete", payload: id });
   };
 
   const handleCompletedNote = (e) => {
+    // const noteId = Number(e.target.value);
+    // setNotes((prevNotes) =>
+    //   prevNotes.map((note) =>
+    //     note.id === noteId ? { ...note, completed: !note.completed } : note
+    //   )
+    // );
     const noteId = Number(e.target.value);
-    setNotes((prevNotes) =>
-      prevNotes.map((note) =>
-        note.id === noteId ? { ...note, completed: !note.completed } : note
-      )
-    );
+    dispatch({ type: "completed", payload: noteId });
   };
-
 
   return (
     <div className="container">
